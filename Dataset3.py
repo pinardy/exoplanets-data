1640b8d34bf2e5fa76c2ad4ce0ee9f81790c31c9
Pinardy <pinardy@hotmail.com>
2017-03-19 11:58:24 +0800

    Added final submission code

---
+++ Dataset3.py
@@ -0,0 +1,88 @@
+# Dataplot for dataset_3
+
+# INSTRUCTIONS: Build and run to obtain the set of graphs saved in your 
+# directory under "\graphs\dataset3\"
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+class DataStruct:
+    def __init__(self, index):
+        self.index = str(index)
+        self.Xarray = []
+        self.Yarray = []
+    def appendX(self, x):
+        self.Xarray.append(x)
+
+    def appendY(self, y):
+        self.Yarray.append(y)
+
+    def plot(self):
+        xv = np.array(self.Xarray)
+        yv = np.array(self.Yarray)
+        plt.figure(self.index)
+        plt.plot(xv, yv)
+        title = "multiple Planet and multiple Orbit Example Light Curve for " + self.index
+        plt.title(title)
+        plt.xlabel('Decimal Phase')
+        plt.ylabel('Relative Flux')
+        plt.savefig(dirPath+self.index+".png")
+
+# Read the file.
+f2 = open('datasets\\dataset_3.txt', 'r')
+
+# read the whole file into a single variable, which is a list of every row of the file.
+lines = f2.readlines()
+f2.close()
+
+# initialize some variable to be lists
+dataStructDict = {}
+dirPath = os.path.dirname(os.path.realpath(__file__)) + "\\graphs\\dataset3\\"
+# initialize directory
+if not os.path.exists(dirPath):
+    os.makedirs(dirPath)
+
+# scan the rows of the file stored in lines, and put the values into some variables
+for line in lines:
+    p = line.split()
+    if (dataStructDict.get(p[0], None) == None):
+        dataStructDict[p[0]] = DataStruct(p[0])
+    dataStructDict[p[0]].appendX(float(p[1]))
+    dataStructDict[p[0]].appendY(float(p[2]))
+
+# iterate through the dictionary to print each orbit results
+for notImportant, dataStruct in dataStructDict.items():
+    dataStruct.plot()
+    minmy=[]
+    minmx=[]
+    findminx=[]
+    findminy=[]
+    minmpointx=[]
+    minmpointy=[]
+    y1=dataStruct.Yarray
+    x1=dataStruct.Xarray
+    for i in range(1,len(y1)-1):
+        if ( (y1[i-1]>y1[i]) and  (y1[i]<y1[i+1]) ):
+            if(y1[i]<0.990):
+                    minmy.append(y1[i])
+                    minmx.append(x1[i])
+    for i in range(1,len(minmy)-1):
+        
+        if(minmx[i]-minmx[i-1]<1):
+            findminx.append(minmx[i-1])
+            findminy.append(minmy[i-1])
+        else:
+            findminx.append(minmx[i-1])
+            findminy.append(minmy[i-1])
+            minpoint=np.min(findminy)
+            index=findminy.index(minpoint)
+            minmpointx.append(findminx[index])
+            minmpointy.append(findminy[index])
+            findminx=[]
+            findminy=[]
+    for i in range(0,len(minmpointx)):
+        print "graph",i, "phase:",minmpointx[i],"flux",minmpointy[i]
+
+# plotting the data
+# plt.show()
---
+++ NewDataset1.py
@@ -0,0 +1,90 @@
+# Dataplot for new_dataset_1
+
+# INSTRUCTIONS: Build and run to obtain the graph for new_dataset_1
+# The time taken for no transit and transit will be printed
+
+
+import matplotlib.pyplot as plt
+import numpy as np 
+from scipy.signal import argrelextrema
+
+# Read the file. 
+f = open('datasets\\new_dataset_1.txt', 'r')
+
+# read the whole file into a single variable, which is a list of every row of the file.
+lines = f.readlines()
+f.close()
+
+# initialize some variable to be lists
+x1 = []
+y1 = []
+phaselist=[]
+
+# scan the rows of the file stored in lines, and put the values into some variables
+for line in lines:
+    p = line.split(",")
+    if(float(p[1])<0.9990):
+        phaselist.append(float(p[0]))
+    x1.append(float(p[0]))
+    y1.append(float(p[1]))
+xv = np.array(x1)
+yv = np.array(y1)
+
+# plotting the data
+plt.plot(xv, yv)
+plt.title('Single Orbit Example Light Curve')
+plt.xlabel('Phase')
+plt.ylabel('Relative Flux')
+plt.show()
+periodpoints=[]
+periodnoorbit=[]
+periodorbit=[]
+
+for i in range(0,len(phaselist)-1):
+    if(phaselist[i+1]-phaselist[i]>1):
+        periodpoints.append(phaselist[i])
+        periodpoints.append(phaselist[i+1])
+
+for i in range(0,len(periodpoints)-1):
+    if(i%2==0):
+        periodnoorbit.append(periodpoints[i+1]-periodpoints[i])
+    else:
+        periodorbit.append(periodpoints[i+1]-periodpoints[i])
+
+print "no eclipse:",np.mean(periodnoorbit)
+print "eclipse:",np.mean(periodorbit)
+print "change in flux:",1-np.min(y1)
+
+# Obtained all of the phases and fluxes for the dips
+
+minmy=[]
+minmx=[]
+findminx=[]
+findminy=[]
+minmpointx=[]
+minmpointy=[]
+
+for i in range(1,len(y1)-1):
+    if ( (y1[i-1]>y1[i]) and  (y1[i]<y1[i+1]) ):
+        if(y1[i]<0.990):
+                minmy.append(y1[i])
+                minmx.append(x1[i])
+
+for i in range(1,len(minmy)-1):
+    
+    if(minmx[i]-minmx[i-1]<1):
+        findminx.append(minmx[i-1])
+        findminy.append(minmy[i-1])
+    else:
+        findminx.append(minmx[i-1])
+        findminy.append(minmy[i-1])
+        minpoint=np.min(findminy)
+        index=findminy.index(minpoint)
+        minmpointx.append(findminx[index])
+        minmpointy.append(findminy[index])
+        findminx=[]
+        findminy=[]
+
+
+for i in range(0,len(minmpointx)):
+    print "phase:",minmpointx[i],"flux",minmpointy[i]
